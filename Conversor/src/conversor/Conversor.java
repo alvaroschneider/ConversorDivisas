4dc10bd78edfcb7b888a2b5ba96baadb2189f3d7
alvaroschneider <alvaro.c.schneider@gmail.com>
2023-04-30 21:12:24 -0300

    Actualizo main

--- Conversor/src/conversor/Conversor.java
+++ Conversor/src/conversor/Conversor.java
@@ -37,6 +37,7 @@ public class Conversor {
         String valorConvertir = "0";
         String opcionElegida;
         double salida;
+        String moneda = " ";
 
         String[] opciones = {"Pesos a Dolar", "Pesos a Euro", "Pesos a Libra", "Pesos a Yen", "Pesos a Won", "Dolar a Pesos", "Euro a Pesos", "Libra a Pesos", "Yen a Pesos", "Won a Pesos"};
 
@@ -48,38 +49,48 @@ public class Conversor {
         switch (opcionElegida) {
             case "Pesos a Dolar":
                 salida = Double.parseDouble(valorConvertir) / dolar;
+                moneda = "dolares";
                 break;
             case "Pesos a Euro":
                 salida = Double.parseDouble(valorConvertir) / euro;
+                moneda = "euros";
                 break;
             case "Pesos a Libra":
                 salida = Double.parseDouble(valorConvertir) / libra;
+                moneda = "libras";
                 break;
             case "Pesos a Yen":
                 salida = Double.parseDouble(valorConvertir) / yen;
+                moneda = "yenes";
                 break;
             case "Pesos a Won":
                 salida = Double.parseDouble(valorConvertir) / won;
+                moneda = "wones";
                 break;
             case "Dolar a Pesos":
                 salida = Double.parseDouble(valorConvertir) * dolar;
+                moneda = "pesos";
                 break;
             case "Euro a Pesos":
                 salida = Double.parseDouble(valorConvertir) * euro;
+                moneda = "pesos";
                 break;
             case "Libra a Pesos":
                 salida = Double.parseDouble(valorConvertir) * libra;
+                moneda = "pesos";
                 break;
             case "Yen a Pesos":
                 salida = Double.parseDouble(valorConvertir) * yen;
+                moneda = "pesos";
                 break;
             case "Won a Pesos":
                 salida = Double.parseDouble(valorConvertir) * won;
+                moneda = "pesos";
                 break;
             default:
                 salida = 0;
         }
-        JOptionPane.showMessageDialog(null, "Divisa convertida: " + salida);
+        JOptionPane.showMessageDialog(null, "Divisa convertida: " + salida + " " + moneda);
     }
 
 }
